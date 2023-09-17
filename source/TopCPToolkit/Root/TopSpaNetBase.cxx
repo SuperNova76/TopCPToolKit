ed18ced0ca70d7513c092233a673095852892e4c
Baptiste Ravina <baptiste.ravina@cern.ch>
2023-09-17 19:54:15 +0200

    maxjets/nfeatures need to be instantiated in each model

--- source/TopCPToolkit/Root/TopSpaNetBase.cxx
+++ source/TopCPToolkit/Root/TopSpaNetBase.cxx
@@ -5,8 +5,6 @@ namespace top {
   TopSpaNetTopology::TopSpaNetTopology(const std::string& name, std::string model_even, std::string model_odd) :
     ONNXWrapper(name, {model_even, model_odd})
   {
-    m_MAX_JETS = m_input_shapes[0][1]; // TODO: Fix this when we fix the batching thing
-    m_NUM_FEATURES = m_input_shapes[0][2];
   }
 
   unsigned TopSpaNetTopology::getSessionIndex(unsigned long long eventNumber) {
