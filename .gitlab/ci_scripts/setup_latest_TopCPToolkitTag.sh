1bce983a11110d87236bf98bf7c011ef2e8f9935
Baptiste Ravina <baptiste.ravina@cern.ch>
2024-03-11 21:57:28 +0100

    Resolve "More data tests for the CI"

--- .gitlab/ci_scripts/setup_latest_TopCPToolkitTag.sh
+++ .gitlab/ci_scripts/setup_latest_TopCPToolkitTag.sh
@@ -6,6 +6,7 @@ echo "        to use the latest release tag...       "
 echo "-----------------------------------------------"
 thisDir=$PWD
 echo "Fetching tag info"
+git config --global --add safe.directory /builds/atlasphys-top/reco/TopCPToolkit
 git fetch --tags
 echo "Getting latest release tag"
 TAG=`git tag | sort --version-sort | tail -n 1`
