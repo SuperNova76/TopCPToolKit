ccf5939d5032a519dba5dad90c838229a7d350f5
Baptiste Ravina <baptiste.ravina@cern.ch>
2024-05-06 14:18:50 +0200

    remove 'name' from ExtraParticleDecorationConfig.py

--- source/TopCPToolkit/python/ExtraParticleDecorationConfig.py
+++ source/TopCPToolkit/python/ExtraParticleDecorationConfig.py
@@ -3,15 +3,14 @@ from AnalysisAlgorithmsConfig.ConfigBlock import ConfigBlock
 class ExtraParticleDecorationConfig(ConfigBlock):
     """ConfigBlock to add generic particle decorations"""
     
-    def __init__(self, name=''):
+    def __init__(self):
         super(ExtraParticleDecorationConfig, self).__init__()
         self.addOption('particles', None, type=str)
-        self.name = name
     
     def makeAlgs(self, config):
         particles = config.readName(self.particles)
 
-        alg = config.createAlgorithm('top::ExtraParticleDecorationAlg', 'ExtraParticleDecoration'+self.name)
+        alg = config.createAlgorithm('top::ExtraParticleDecorationAlg', 'ExtraParticleDecoration'+self.particles)
         alg.particles = particles
 
         config.addOutputVar(self.particles, 'e_%SYS%', 'e')
