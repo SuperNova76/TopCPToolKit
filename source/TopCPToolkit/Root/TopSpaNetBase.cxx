006360ac48170969e09a643e3532e1479b2524b1
Steffen Korn <steffen.korn@cern.ch>
2024-07-31 17:32:59 +0200

    Adding SpaNet references and some comments

--- source/TopCPToolkit/Root/TopSpaNetBase.cxx
+++ source/TopCPToolkit/Root/TopSpaNetBase.cxx
@@ -1,7 +1,8 @@
 #include "TopSpaNet/TopologyBase.h"
 
+// Reference for SpaNet: https://arxiv.org/pdf/2106.03898
+// Reference for SpaNet including regression/classification heads: https://www.nature.com/articles/s42005-024-01627-4
 namespace top {
-
   TopSpaNetTopology::TopSpaNetTopology(const std::string& name, const std::string& model_even, const std::string& model_odd) :
     ONNXWrapper(name, {model_even, model_odd}),
     m_NUM_FEATURES(-1),
