2870b225066362a1d0406706d0af4a61b1bec8f7
Matthew Leigh <matthew.leigh@cern.ch>
2024-09-19 11:43:51 +0200

    reverted vscode and entries

--- source/TopCPToolkit/src/components/TopCPToolkit_entries.cxx
+++ source/TopCPToolkit/src/components/TopCPToolkit_entries.cxx
@@ -2,35 +2,32 @@
   Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
-#include <TopCPToolkit/BTaggingScoresAlg.h>
-#include <TopCPToolkit/CARAlg.h>
-#include <TopCPToolkit/DiTauMassCalculatorAlg.h>
-#include <TopCPToolkit/FakeBkgCalculatorAlg.h>
-#include <TopCPToolkit/JetMatchingAlg.h>
-#include <TopCPToolkit/JetReclusteringAlg.h>
 #include <TopCPToolkit/KLFitterAlg.h>
 #include <TopCPToolkit/KLFitterFinalizeOutputAlg.h>
-#include <TopCPToolkit/LeptonSFCalculatorAlg.h>
 #include <TopCPToolkit/ParticleLevelAlg.h>
 #include <TopCPToolkit/PartonToJetsMatchAlg.h>
-#include <TopCPToolkit/RunNuFlowsAlg.h>
 #include <TopCPToolkit/RunPartonHistoryAlg.h>
+#include <TopCPToolkit/CARAlg.h>
 #include <TopCPToolkit/RunSpaNetAlg.h>
+#include <TopCPToolkit/RunNuFlowsAlg.h>
 #include <TopCPToolkit/RunTtbarNNLORecursiveRewAlg.h>
-#include <TopCPToolkit/SVMassAlg.h>
-#include <TopCPToolkit/VGammaORAlg.h>
+#include <TopCPToolkit/JetMatchingAlg.h>
+#include <TopCPToolkit/SoftMuonSelectorAlg.h>
+#include <TopCPToolkit/PartonHistoryToSpinInputAlg.h>
+#include <TopCPToolkit/TopSpinDensityMatrixAlg.h>
+#include <TopCPToolkit/SinglelepHadronicChi2RecoAlg.h>
 
-DECLARE_COMPONENT(top::RunTtbarNNLORecursiveRewAlg)
-DECLARE_COMPONENT(top::RunSpaNetAlg)
-DECLARE_COMPONENT(top::RunNuFlowsAlg)
-DECLARE_COMPONENT(top::RunPartonHistoryAlg)
-DECLARE_COMPONENT(top::CARAlg)
-DECLARE_COMPONENT(top::PartonToJetsMatchAlg)
-DECLARE_COMPONENT(top::ParticleLevelAlg)
-DECLARE_COMPONENT(top::KLFitterFinalizeOutputAlg)
-DECLARE_COMPONENT(top::KLFitterAlg)
-DECLARE_COMPONENT(top::JetMatchingAlg)
-DECLARE_COMPONENT(top::SoftMuonSelectorAlg)
-DECLARE_COMPONENT(top::PartonHistoryToSpinInputAlg)
-DECLARE_COMPONENT(top::TopSpinDensityMatrixAlg)
-DECLARE_COMPONENT(top::SinglelepHadronicChi2RecoAlg)
+DECLARE_COMPONENT (top::RunTtbarNNLORecursiveRewAlg)
+DECLARE_COMPONENT (top::RunSpaNetAlg)
+DECLARE_COMPONENT (top::RunNuFlowsAlg)
+DECLARE_COMPONENT (top::RunPartonHistoryAlg)
+DECLARE_COMPONENT (top::CARAlg)
+DECLARE_COMPONENT (top::PartonToJetsMatchAlg)
+DECLARE_COMPONENT (top::ParticleLevelAlg)
+DECLARE_COMPONENT (top::KLFitterFinalizeOutputAlg)
+DECLARE_COMPONENT (top::KLFitterAlg)
+DECLARE_COMPONENT (top::JetMatchingAlg)
+DECLARE_COMPONENT (top::SoftMuonSelectorAlg)
+DECLARE_COMPONENT (top::PartonHistoryToSpinInputAlg)
+DECLARE_COMPONENT (top::TopSpinDensityMatrixAlg)
+DECLARE_COMPONENT (top::SinglelepHadronicChi2RecoAlg)
